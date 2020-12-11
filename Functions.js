331b9f4b66b738d561d9f47de3b82bdce9b60f4d
Glitch (hello-express) <none>
2020-12-11 15:09:19 +0000

    💑🗾 Checkpoint ./Functions.js:13134877/2411

--- Functions.js
+++ Functions.js
@@ -1,5 +1,5 @@
 module.exports = {
-  GetRegxp: async function(Type) {
+  async GetRegxp(Type) {
     if (!Type || typeof Type !== "string") return null;
 
     if (Type === "YtID") {
@@ -12,14 +12,14 @@ module.exports = {
       return /https?:\/\/(www.)?youtube.com\/playlist\?list=((PL|UU|LL|RD)[a-zA-Z0-9-_]{16,41})/;
     }
   },
-  Linker: async function(Type) {
+  async Linker(Type) {
     if (Type.toLowerCase() === "Base") {
       return "https://youtube.com/watch?v=";
     } else {
       return `https://youtube.com/watch?v=${Type}`;
     }
   },
-  Player: async function(message, Discord, client, Ytdl, options = {}) {
+  async Player(message, Discord, client, Ytdl, options = {}) {
     const Filters = {
       bassboost: "bass=g=20,dynaudnorm=f=200",
       "8D": "apulsator=hz=0.08",
@@ -117,80 +117,7 @@ module.exports = {
       Db.TextChannnel.send(`🎶 Now Playing: **${options.Play.Title}**`)
     );
   },
-  UpdateStream: async function(message, client, Filter, ytdl) {
-    const Queue = await client.queue.get(message.guild.id);
-
-    const Filters = {
-      bassboost: "bass=g=20,dynaudnorm=f=200",
-      "8D": "apulsator=hz=0.08",
-      vaporwave: "aresample=48000,asetrate=48000*0.8",
-      nightcore: "aresample=48000,asetrate=48000*1.25",
-      phaser: "aphaser=in_gain=0.4",
-      tremolo: "tremolo",
-      vibrato: "vibrato=f=6.5",
-      reverse: "areverse",
-      treble: "treble=g=5",
-      normalizer: "dynaudnorm=f=200",
-      surrounding: "surround",
-      pulsator: "apulsator=hz=1",
-      subboost: "asubboost",
-      karaoke: "stereotools=mlev=0.03",
-      flanger: "flanger",
-      gate: "agate",
-      haas: "haas",
-      mcompand: "mcompand"
-    };
-
-    const EcoderFilters = [];
-    Object.keys(Queue.Filters).forEach(FilterName => {
-      if (Queue.Filters[FilterName]) {
-        EcoderFilters.push(Filters[FilterName]);
-      }
-    });
-    let Encoder;
-    if (EcoderFilters.length < 1) {
-      Encoder = [];
-    } else {
-      Encoder = ["-af", EcoderFilters.join(",")];
-    }
-
-    const Old = Queue.Bot.dispatcher.streamTime;
-
-    const Stream = ytdl(Queue.Songs[0].Link, {
-      filter: "audioonly",
-      opusEncoded: true,
-      Encoder,
-      seek: Queue.Bot.dispatcher.streamTime / 1000,
-      highWaterMark: 1 << 30
-    });
-
-    setTimeout(async () => {
-      if (Queue.Bot) Queue.Bot.dispatcher.destroy();
-      Queue.Bot.dispatcher = Stream;
-      Queue.Bot.play(Stream, {
-        type: "opus",
-        bitrate: "auto"
-      })
-        .on("finish", async () => {
-          const Shift = await Queue.Songs.shift();
-          if (Queue.Loop === true) {
-            await Queue.Songs.push(Shift);
-          }
-          //await Player(Queue.Songs[0]);
-        })
-        .on("error", async error => {
-          await console.log(error);
-          return message.channel.send(
-            "Error: Something Went Wrong From Bot Inside"
-          );
-        });
-
-      Queue.Bot.dispatcher.streamTime += Old;
-
-      await Queue.Bot.dispatcher.setVolumeLogarithmic(Queue.Volume / 100);
-    }, 1000);
-  },
-  Objector: async function(Song, message) {
+  async Objector(Song, message) {
     function FD(duration) {
       let minutes = Math.floor(duration / 60);
       let hours = "";
@@ -209,30 +136,29 @@ module.exports = {
     }
     async function FC(Count) {
       if (Count.length === 4) {
-        return `${Count[0]}k`;
+        return `${Count[0]} Thousand`;
       } else if (Count.length === 5) {
-        return `${Count[0]}${Count[1]}k`;
+        return `${Count[0]}${Count[1]} Thousand`;
       } else if (Count.length === 6) {
-        return `${Count[0]}${Count[1]}${Count[2]}k`;
+        return `${Count[0]}${Count[1]}${Count[2]} Thousand`;
       } else if (Count.length === 7) {
-        return `${Count[0]}m`;
+        return `${Count[0]} Million`;
       } else if (Count.length === 8) {
-        return `${Count[0]}${Count[1]}m`;
+        return `${Count[0]}${Count[1]} Million`;
       } else if (Count.length === 9) {
-        return `${Count[0]}${Count[1]}${Count[2]}m`;
+        return `${Count[0]}${Count[1]}${Count[2]} Million`;
       } else if (Count.length === 10) {
-        return `${Count[0]}b`;
+        return `${Count[0]} Billion`;
       } else if (Count.length === 11) {
-        return `${Count[0]}${Count[1]}b`;
+        return `${Count[0]}${Count[1]} Billion`;
       } else if (Count.length === 12) {
-        return `${Count[0]}${Count[1]}${Count[2]}b`;
+        return `${Count[0]}${Count[1]}${Count[2]} Billion`;
       } else if (Count.length === 13) {
-        return `${Count[0]}t`;
+        return `${Count[0]} Trillion`;
       } else {
         return Count;
-      }
-    }
-    ("");
+      };
+    };
     return {
       ID: Song.videoId,
       Title: Song.title,
