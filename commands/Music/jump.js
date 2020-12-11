3c21d6b65a93971c68f640af58312e334b6462d1
Glitch (hello-express) <none>
2020-12-11 01:39:37 +0000

    📡⛪️ Checkpoint ./commands/Music/jump.js:13134877/5159

--- commands/Music/jump.js
+++ commands/Music/jump.js
@@ -9,27 +9,46 @@ module.exports = {
   description: "Jump To Your Best Song!",
   usage: "Jump <Number Or Title>",
   run: async (client, message, args) => {
-    
     const Channel = message.member.voice.channel;
-    
+
     if (!Channel) return message.channel.send("Please Join A Voice Channel!");
-    
+
     const Queue = await client.queue.get(message.guild.id);
-    
-    if (!Queue) return message.channel.send("Nothing Is Playing Right Now, Add Some Songs To Queue :D");
-    
+
+    if (!Queue)
+      return message.channel.send(
+        "Nothing Is Playing Right Now, Add Some Songs To Queue :D"
+      );
+
     const Content = args.join(" ");
-    
-    if (isNaN(Content)) {
-      const Song = await Queue.Songs.find()
-    }
-    
+
     const Embed = new Discord.MessageEmbed()
-    .setColor(Color)
-    .setTitle("Success")
-    .setDescription("🎶 Joined The Voice Channel, Use Play Command To Play Music!")
-    .setTimestamp();
-    
-    return message.channel.send(Embed).catch(() => message.channel.send("🎶 Joined The Voice Channel, Use Play Command To Play Music!"));
+      .setColor(Color)
+      .setTitle("Success")
+      .setDescription("🎶 Successfully Jumped")
+      .setTimestamp();
+
+    if (isNaN(Content)) {
+      const Song = await Queue.Songs.find(
+        Son => Son.Title === Content.toLowerCase()
+      );
+      if (!Song) return message.channel.send("No Song Found!");
+      await Queue.Songs.splice(
+        0,
+        Math.floor(parseInt(Queue.Songs.indexOf(Song) + 1))
+      );
+      await Queue.Bot.dispatcher.end();
+      return message.channel
+        .send(Embed)
+        .catch(() => message.channel.send("🎶 Successfully Jumped"));
+    } else {
+      const Song = await Queue.Songs[Content];
+      if (!Song) return message.channel.send("No Song Found!");
+      await Queue.Songs.splice(0, Math.floor(parseInt(Content - 1)));
+      await Queue.Bot.dispatcher.end();
+      return message.channel
+        .send(Embed)
+        .catch(() => message.channel.send("🎶 Successfully Jumped"));
+    }
   }
 };
