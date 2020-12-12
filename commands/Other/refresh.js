e431be793dc78a9310347cecab956457215057bf
Glitch (hello-express) <none>
2020-12-12 18:45:09 +0000

    ⛱🌋 Checkpoint ./commands/Other/refresh.js:13134877/1383 ./server.js:13134877/9

--- commands/Other/refresh.js
+++ commands/Other/refresh.js
@@ -9,6 +9,19 @@ module.exports = {
   description: "Refresh Bot For Better Performance!",
   usage: "refresh",
   run: async (client, message, args) => {
-     if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`You Don't Have E`)
+     if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`You Don't Have Enough Permission To Use This Command - Administrator`);
+    
+    const Filter = m => m.author.id == message.author.id;
+    
+    const Warn = await message.channel.send("Please Respond With Yes Or No To Confirm Refresh!\n\nIt Will Clear Queue But It Will Make Quality Better\n\nTime Limit: 5 Minutes");
+    
+    await message.channel.awaitMessages(Filter, { max: 1, time: 300000, errors: ["time"] }).then(async Msg => {
+        Msg = Msg.content.toLowerCase();
+        if (Msg === "no" || Msg !== "no" && Msg !== "yes") return message.channel.send("Okay, Canceled >_<");
+        if (message.guild.me.voice) await message.guild.me.voice.kick(message.guild.me.id);
+        await client.queue.delete(message.guild.id);
+        const Embed = new Discord.MessageEmbed()
+        .setColor(C)
+    });
   }
 };
