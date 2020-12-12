c3da0b150dfe80443f1e67cec9c2b4d115791417
Glitch (hello-express) <none>
2020-12-12 20:04:25 +0000

    ⚓️🏏 Checkpoint ./commands/Music/removesong.js:13134877/1244

---
+++ commands/Music/removesong.js
@@ -0,0 +1,36 @@
+const { Default_Prefix, Color } = require("../../config.js");
+const Discord = require("discord.js");
+const db = require("wio.db");
+
+module.exports = {
+  name: "removesong",
+  aliases: ["rs"],
+  category: "Music",
+  description: "Remove A Song From Queue!",
+  usage: "Removesong <Title Or Number [From Queue]>",
+  run: async (client, message, args) => {
+    
+    const Channel = message.member.voice.channel;
+    
+    if (!Channel) return message.channel.send("Please Join A Voice Channel!");
+    
+    const Queue = await client.queue.get(message.guild.id);
+    
+    if (!Queue) return message.channel.send("Nothing Is Playing Right Now, Add Some Songs To Queue :D");
+   
+    if (Queue.Songs.length === 1) return message.channel.send("🎶 Only 1 Song In Queue :(");
+    
+    
+    
+    Queue.Playing = false;
+    Queue.Bot.dispatcher.pause();
+    
+    const Embed = new Discord.MessageEmbed()
+    .setColor(Color)
+    .setTitle("Success")
+    .setDescription("🎶 Music Has Been Paused!")
+    .setTimestamp();
+    
+    return message.channel.send(Embed).catch(() => message.channel.send("🎶 Music Has Been Paused!"));
+  }
+};
