f9c3146f24b849020e32c2acb90b8e98372de62a
Glitch (truth-seed-amber) <none>
2020-12-20 04:11:22 +0000

    💸🏘 Checkpoint ./config.js:24282868/78 ./server.js:24282868/17 ./commands/Other/help.js:24282868/88

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ exports.Default_Prefix = "ms"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = `https://discord.gg/NZwwSUx8es`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
 exports.Donate = `https://www.youtube.com/channel/UCpLctSYrYYQm58ul219kMwg`; //Please Don't Change It, If You Want To Support Me <3
+exports.Dashboard = `https://mire-general-close.glitch.me/`
